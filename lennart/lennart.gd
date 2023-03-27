96685e4f256e6a1ca2c90fdb74d38a75a8f5f703
FelixJ <feljon@student.chalmers.se>
2023-03-27 21:28:25 +0200

    Platforms gets deleted when exiting screen

--- lennart/lennart.gd
+++ lennart/lennart.gd
@@ -23,15 +23,35 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_pressed("ui_accept"):
+		print("pressed")
+		velocity = Vector2(0,0)
+		position = get_viewport_center_position()
 	
-	if direction:
+	if direction != 0:
 		velocity.x = direction * Speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, Speed)
 
+	
 	move_and_slide()
+	
+	position.x = wrapf(position.x, 0, 180)
 	
 	# Check if player is moving upwards
 	if velocity.y < 0:
 		pass
+
+
+func get_viewport_center_position() -> Vector2:
+	var viewport = get_viewport()
+	var camera = viewport.get_camera_2d()
+	var viewport_center_position = viewport.get_visible_rect().size / 2
+	
+	# This gets the overwritten window size values!
+	print(viewport.get_visible_rect().size)
+
+
+	return viewport_center_position
+
 
--- platforms/platform.gd
+++ platforms/platform.gd
@@ -1,2 +1,7 @@
-extends CharacterBody2D
+extends StaticBody2D
 
+func _ready() -> void:
+	get_node("VisibleOnScreenNotifier2D").screen_exited.connect(on_screen_exited)
+	
+func on_screen_exited() -> void:
+	queue_free()
