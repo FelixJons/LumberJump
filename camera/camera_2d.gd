16a7904c3c04283f78f15bdf59209080a3f2d90f
FelixJ <feljon@student.chalmers.se>
2023-03-27 19:44:07 +0200

    Added random spawning platforms and camera

--- camera/camera_2d.gd
+++ camera/camera_2d.gd
@@ -5,7 +5,6 @@ var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(player_path)
-	set_process(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
